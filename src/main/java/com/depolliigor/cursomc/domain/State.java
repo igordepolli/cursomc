4fc33dabf902ae45d64482ca330c5ade3ce8d386
Igor Depolli <igorcler@gmail.com>
2021-01-18 19:31:44 -0300

    Change references Json

--- src/main/java/com/depolliigor/cursomc/domain/State.java
+++ src/main/java/com/depolliigor/cursomc/domain/State.java
@@ -3,13 +3,14 @@ package com.depolliigor.cursomc.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class State implements Serializable {
@@ -20,7 +21,7 @@ public class State implements Serializable {
 	private Integer id;
 	private String name;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="state")
 	private List<City> cities = new ArrayList<>();
 	
