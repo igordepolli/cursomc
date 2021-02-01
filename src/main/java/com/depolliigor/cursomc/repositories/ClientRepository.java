77478c8bd5c9337c26fe8d11005d57bdf7faa627
Igor Depolli <igorcler@gmail.com>
2021-02-01 20:42:29 -0300

    Repeated email validation

--- src/main/java/com/depolliigor/cursomc/repositories/ClientRepository.java
+++ src/main/java/com/depolliigor/cursomc/repositories/ClientRepository.java
@@ -2,10 +2,14 @@ package com.depolliigor.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.depolliigor.cursomc.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
+	
+	@Transactional(readOnly=true)
+	Client findByEmail(String email);
 	
 }
--- src/main/java/com/depolliigor/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/depolliigor/cursomc/services/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.depolliigor.cursomc.domain.Client;
 import com.depolliigor.cursomc.domain.enums.ClientType;
 import com.depolliigor.cursomc.dto.ClientNewDTO;
+import com.depolliigor.cursomc.repositories.ClientRepository;
 import com.depolliigor.cursomc.resources.exceptions.FieldMessage;
 import com.depolliigor.cursomc.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	
+	@Autowired
+	private ClientRepository repo;
 	
 	@Override
 	public void initialize(ClientInsert ann) {
@@ -27,6 +34,10 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		
 		if (objDto.getClientType().equals(ClientType.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOrCnpj()))
 			list.add(new FieldMessage("cpfOrCnpj", "CNPJ inválido!"));
+		
+		Client aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null)
+			list.add(new FieldMessage("email", "E-mail já existe!"));
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
